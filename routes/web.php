11504af88406dba834d30acd28885986863b5646
Dom Elves <dom_elves@hotmail.co.uk>
2025-03-04 10:54:22 +0000

    basic form, routes, empty controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrlController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/encode', [UrlController::class, 'encode'])->name('encode');
+Route::post('/decode', [UrlController::class, 'decode'])->name('decode');
